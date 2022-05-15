4e41c57c09a1ef9389e6ce8e36534a65b9454fa9
Aviv Haruzi <avivharuzi@gmail.com>
2022-05-15 08:57:11 +0300

    add post entity to the posts module

--- apps/api/src/app/posts/posts.module.ts
+++ apps/api/src/app/posts/posts.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostEntity } from './post.entity';
 
 import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([PostEntity])],
   providers: [PostsService],
   controllers: [PostsController],
 })
